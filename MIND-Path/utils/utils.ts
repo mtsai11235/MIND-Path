a7147c79cea6aada58c9b4302e79818e806cb638
Max Tsai <maxtsai@brandeis.edu>
2025-11-17 13:49:36 -0500

    working dev build

--- MIND-Path/utils/utils.ts
+++ MIND-Path/utils/utils.ts
@@ -1,7 +1,7 @@
 import Constants from 'expo-constants';
 
 export const generateAPIUrl = (relativePath: string) => {
-  const origin = Constants.experienceUrl.replace('exp://', 'http://');
+  const origin = Constants.experienceUrl ? Constants.experienceUrl.replace('exp://', 'http://') : 'http://localhost:8081';
 
   const path = relativePath.startsWith('/') ? relativePath : `/${relativePath}`;
 
